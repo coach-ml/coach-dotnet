2d8835f7474a7f13464156323d441031c5e27e13
Loren Kuich <loren.jk3@gmail.com>
2019-07-06 15:13:53 -0700

    Init commit

---
+++ Coach.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Coach {
+    class CoachModel {
+        
+    }
+
+    public class Profile {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("bucket")]
+        public string Bucket { get; set; }
+
+        [JsonProperty("models")]
+        public dynamic Models { get; set; }
+    }
+
+    public class CoachClient {
+        
+        public bool IsDebug { get; private set; }
+        private Profile Profile { get; set; }
+        private string ApiKey { get; set; }
+
+        public CoachClient(bool isDebug = false) {
+            this.IsDebug = isDebug;
+        }
+
+        public async Task Login(string apiKey) {
+            this.ApiKey = apiKey;
+            this.Profile = await GetProfile();
+        }
+
+        private bool IsAuthenticated() {
+            return this.Profile != null;
+        }
+
+        public async Task CacheModel(string name, string path=".") {
+            if (!IsAuthenticated())
+                throw new Exception("User is not authenticated");
+
+            // Create the target dir
+            if (!Directory.Exists(name))
+                Directory.CreateDirectory(name);
+
+            int profileVersion = this.Profile.Models[name].version;
+            string profileManifest = $"{path}/{name}/manifest.json";
+
+            if (File.Exists(profileManifest)) {
+                // Load existing model manifest
+                dynamic manifest = JsonConvert.DeserializeObject(File.ReadAllText(profileManifest));
+                int manifestVersion = manifest[name].version;
+
+                if (profileVersion == manifestVersion) {
+                    if (this.IsDebug) {
+                        Console.WriteLine("Version match, skipping model download");
+                    }
+
+                    return;
+                }
+            } else {
+                // Write profile to local manifest
+                var model = this.Profile.Models[name];
+                
+                dynamic parent = JsonConvert.DeserializeObject("{}");
+                parent[name] = model;
+
+                var json = JsonConvert.SerializeObject(parent);
+                File.WriteAllText(profileManifest, json);
+            }
+
+            var baseUrl = $"https://la41byvnkj.execute-api.us-east-1.amazonaws.com/prod/{this.Profile.Bucket}/model-bin?object=trained/{name}/{profileVersion}/model";
+                    
+            var modelFile = "frozen.pb";
+            var modelUrl = $"{baseUrl}/{modelFile}";
+
+
+            var request = new HttpClient();
+            request.DefaultRequestHeaders.Add("X-Api-Key", this.ApiKey);
+            request.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+            
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, modelUrl);
+            requestMessage.Content = new StringContent(String.Empty, Encoding.UTF8, "application/octet-stream");
+            
+            var response = await request.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+
+            byte[] modelBytes = await response.Content.ReadAsByteArrayAsync();
+            await File.WriteAllBytesAsync($"{path}/{name}/{modelFile}", modelBytes);
+        }
+
+        private async Task<Profile> GetProfile() {
+            var id = this.ApiKey.Substring(0, 5);
+            var url = $"https://2hhn1oxz51.execute-api.us-east-1.amazonaws.com/prod/{id}";
+
+            var request = new HttpClient();
+            request.DefaultRequestHeaders.Add("X-Api-Key", this.ApiKey);
+
+            var response = await request.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var profile = JsonConvert.DeserializeObject<Profile>(responseBody);
+            return profile;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Coach;
+
+namespace CoachSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login().Wait();
+            Console.WriteLine("Done");
+        }
+
+        static async Task Login() {
+            var c = new CoachClient();
+            await c.Login("");
+            await c.CacheModel("flowers");
+        }
+    }
+}
